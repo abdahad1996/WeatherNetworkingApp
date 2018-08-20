f2ae3bac351f695d269d4e55799577b8d80fbaf6
Admin <admin@Admins-iMac.local>
2018-08-20 21:10:37 +0500

    first commit

---
+++ weatherapp/ChangeWeatherVC.swift
@@ -0,0 +1,37 @@
+//
+//  ChangeWeatherVC.swift
+//  weatherapp
+//
+//  Created by Admin on 20/08/2018.
+//  Copyright © 2018 abdulahad. All rights reserved.
+//
+
+import UIKit
+
+class ChangeWeatherVC: UIViewController {
+
+    
+
+    @IBOutlet weak var changeCityTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func getWeatherPressed(_ sender: Any) {
+        
+        
+        
+    }
+}
---
+++ weatherapp/WeatherVC.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Admin on 20/08/2018.
+//  Copyright © 2018 abdulahad. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+    
+
+
+}
+
